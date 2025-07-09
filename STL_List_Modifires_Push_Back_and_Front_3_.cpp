b89a4afedd761700afe2ac3cffb08615b60d10ad
Rahat Kabir Ifty Mac <iftyrahatkabir@gmail.com>
2025-07-09 18:40:48 +0600

    Push back & front

---
+++ STL_List_Modifires_Push_Back_and_Front_3_.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+    list<int> l = {10, 20, 30};
+    
+    l.push_back(40);
+    l.push_front(100);
+
+    for(int val : l)
+    {
+        cout << val << endl;
+    }
+    
+
+    return 0;
+}
